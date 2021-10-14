27a26bce120d53a0f29386077e2a80a8901eda84
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-14 19:25:34 -0300

    add validador de novos usuarios

---
+++ helpers/validateFields.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+const CustomError = require('./CustomError');
+
+const newUser = (user) => {
+  const { error } = Joi.object({
+    displayName: Joi.string().min(8).not().empty()
+      .required(),
+    email: Joi.string().email().not().empty()
+      .required(), 
+    password: Joi.string().length(6).not().empty()
+      .required(),
+    image: Joi.string().allow(null, ''),
+  }).validate(user);
+
+  if (error) throw new CustomError(error.message, 400);
+};
+
+module.exports = {
+  newUser,
+};
