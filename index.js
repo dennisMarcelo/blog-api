76c2186a0c483a443f78f5faa22f2e2ab4f6d1b0
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-14 19:34:13 -0300

    index configurado

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const userController = require('./controllers/userController');
+const errorMiddleware = require('./middlewares/error');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/user', userController);
+
+app.use(errorMiddleware);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
