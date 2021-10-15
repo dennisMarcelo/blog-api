073bb50486a11c884248e94de7daaf65277179d1
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-15 11:16:47 -0300

    service para add novas categorias

---
+++ services/categoryService.js
@@ -0,0 +1,19 @@
+const { Categorie } = require('../models');
+const CustomError = require('../helpers/CustomError');
+
+const create = async (newCategory) => {
+  const categoryExist = await Categorie.findOne({
+    where: {
+      name: newCategory.name,
+    },
+  });
+
+  if (categoryExist) throw new CustomError('Category already registered', 409);
+
+  const categoryCreated = await Categorie.create(newCategory);
+  return categoryCreated;
+};
+
+module.exports = {
+  create,
+};
