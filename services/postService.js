76fccd251f75a2b3dea840f5c606b859a1639dec
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-15 20:17:25 -0300

    add service para criar novo post

---
+++ services/postService.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const CustomError = require('../helpers/CustomError');
+
+const { BlogPost, PostsCategorie } = require('../models');
+const config = require('../config/config');
+
+const sequelize = new Sequelize(config.development);
+
+const create = async (post, user) => {
+  const { title, content, categoryIds } = post;
+  const { id } = user;
+  try {
+    return await sequelize.transaction(async (t) => {
+       const postCreated = await BlogPost.create(
+        { title, content, userId: id, published: new Date(), updated: new Date() },
+        { transaction: t },
+      );
+      
+      const relationOfTagPost = categoryIds
+        .map((categoryId) => ({ postId: postCreated.id, categoryId }));
+      
+      await PostsCategorie.bulkCreate(relationOfTagPost, { transaction: t });
+      
+      return postCreated;
+    });
+  } catch (err) {
+    throw new CustomError('"categoryIds" not found', 400);
+  }
+};
+
+module.exports = {
+  create,
+};
