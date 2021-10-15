f5cf90b024bed58d03e50300a32b0227fa4f1dc8
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-14 23:49:35 -0300

    add controller para obter todos usuários

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const rescue = require('express-rescue');
 
 const isValid = require('../helpers/validateFields');
 const userService = require('../services/userService');
+const { validateJWT } = require('../middlewares/authorization');
 
 const router = express.Router();
 
@@ -12,6 +13,12 @@ router.post('/', rescue(async (req, res) => {
   const token = await userService.create(req.body);
   
   res.status(201).json({ token });
+}));
+
+router.get('/', validateJWT, rescue(async (req, res) => {
+  const users = await userService.getAll();
+
+  res.status(200).json(users);
 }));
 
 module.exports = router;
