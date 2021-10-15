414e577fd2ab8e51016b576b4c032b4a0cfb65a9
dennisMarcelo <dennismarcelo15@gmail.com>
2021-10-15 19:01:40 -0300

    atualizando model de post

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -5,6 +5,8 @@ module.exports = (sequelize, DataTypes) => {
       title: DataTypes.STRING,
       content: DataTypes.STRING,
       userId: DataTypes.INTEGER,
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
     },
     { timestamps: false, tableName: 'BlogPosts' });
 
